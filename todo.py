b82dbcebcf9ee537b3c5c5e44aa743cba6d0fde7
sinneon <sinneon.world@gmail.com>
2024-05-15 14:48:08 +0900

    ToDo_second

--- todo.py
+++ todo.py
@@ -5,7 +5,7 @@ from tkinter import filedialog
 from PIL import Image, ImageTk
 
 app = tk.Tk()
-app.geometry('300x400')
+app.geometry('300x300')
 app.title('ToDoList')
 
 lb1_1 = tk.Label(text='日付と予定を入力してください')
@@ -28,9 +28,10 @@ todofile = 'todo.txt'
 
 #呼び出し
 def load_todo():
+    global todo1
     if os.path.exists(todofile):
         with open(todofile, 'r') as file:
-            todo = file.readlines()
+            todo1 = file.readlines()
         todo1 = [todo.strip() for todo in todo1]
     else:
         todo1 = []
@@ -43,8 +44,8 @@ def save_todo(todo1):
             file.write(todo + '\n')
 
 #予定を追加
-def add_todo(todo1, item):
-    todo1.append(item)
+def add_todo(todo1, text):
+    todo1.append(text)
     save_todo(todo1)
 
 #予定を削除
@@ -53,40 +54,23 @@ def delete_todo(todo1, index):
         todo1.pop(index)
         save_todo(todo1)
 
-'''
 def main():
-    todo1 = load_todo()
+    todos = load_todo()
 
 if __name__ == '__main__':
     main()
 
-    while True:
-        command = input("コマンドを入力してください（追加、表示、削除、終了）: ")
-        if command == '終了':
-            break
-        elif command == '追加':
-            item = input("ToDo項目を入力してください: ")
-            add_todo(todo1, item)
-        elif command == '表示':
-            for i, todo in enumerate(todo1):
-                print(f"{i}: {todo}")
-        elif command == '削除':
-            index = int(input("削除するToDoの番号を入力してください: "))
-            delete_todo(todo1, index)
-        else:
-            print("無効なコマンドです。")   
-'''
-
-#予定を入力
-#btn1 = tk.Button(app, text='予定を入力', command=save_todo)
-#btn1.place(x=30, y=120)
 
 #予定を表示
-btn2 = tk.Button(app, text='予定を出力', command=load_todo)
-btn2.place(x=30, y=180)
+def btn_click1():
+    for i, todo in enumerate(todo1):
+        print(f"{i}: {todo}")
+
+btn1 = tk.Button(app, text='予定を出力', command=btn_click1)
+btn1.place(x=30, y=140)
 
 #予定を追加
-def btn_click1():
+def btn_click2():
     global todo_time
     global todo_schedule
     global todo2
@@ -96,26 +80,16 @@ def btn_click1():
     lb2_2 = tk.Label(text=f'予定: {todo_schedule}')
     lb2_2.place(x=130, y=100)
     text = f'{todo_time}' + ': ' +  f'{todo_schedule}'
-    file = open(todofile, 'w')
-    file.write(text)
-    file.close()
+    add_todo(todo1, text)
 
-
-btn3 = tk.Button(app, text='予定を追加', command=btn_click1)
-btn3.place(x=30, y=240)
+btn2 = tk.Button(app, text='予定を追加', command=btn_click2)
+btn2.place(x=30, y=170)
 
 #予定を削除
-btn4 = tk.Button(app, text='予定を削除', command=delete_todo)
-btn4.place(x=30, y=300)
+def btn_click3():
+    index = int(input("削除するToDoの番号を入力してください: "))
+    delete_todo(todo1, index)
+btn3 = tk.Button(app, text='予定を削除', command=btn_click3)
+btn3.place(x=30, y=200)
 
 app.mainloop()
-
-
-'''
-現在の状況メモ
-GUIへの以降が難しい・・・。
-予定を１行テキストに追加することはできたが
-次に追加しようとすると上書きになって消えてしまう。
-
-予定の表示、削除についてはまだボタンだけ作成
-'''
